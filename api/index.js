6eae8e4a2cc6d3b236f39ffd52e8aa10943354d6
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-12 01:41:58 +0300

    push

--- api/index.js
+++ api/index.js
@@ -4,20 +4,35 @@ import dotenv from 'dotenv';
 import { createAdmin } from '../controllers/authController.js';
 dotenv.config();
 
-async function App() {
+// async function App() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected");
+//     await createAdmin();
+//     console.log("createAdmin passed");
+//     app.listen(3000, () => {
+//       console.log("Server ready on port 3000.");
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// App();
+
+// export default app;
+
+export default async function handler(req, res) {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
+    
     await createAdmin();
     console.log("createAdmin passed");
-    app.listen(3000, () => {
-      console.log("Server ready on port 3000.");
-    });
+    
+    res.status(200).json({ message: 'Server ready on Vercel' });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
-
-App();
-
-export default app;
