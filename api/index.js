63b80cd69d2d270e4f33baf55aeb9e2b3900af82
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-12 01:47:54 +0300

    push

--- api/index.js
+++ api/index.js
@@ -1,40 +1,23 @@
 import app from '../app.js';
-import serverless from 'serverless-http';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { createAdmin } from '../controllers/authController.js';
 dotenv.config();
 
-// async function App() {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log("MongoDB connected");
-//     await createAdmin();
-//     console.log("createAdmin passed");
-//     app.listen(3000, () => {
-//       console.log("Server ready on port 3000.");
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// App();
-
-// export default app;
-
-export default async function handler(req, res) {
+async function App() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
-
     await createAdmin();
     console.log("createAdmin passed");
-
-    const response = await serverless(app)(req, res);
-    return response;
+    app.listen(3000, () => {
+      console.log("Server ready on port 3000.");
+    });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(error);
   }
 }
+
+App();
+
+export default app;
