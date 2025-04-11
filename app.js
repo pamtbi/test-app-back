d3f052cf7377f677459cfc61669bf6e8633b1555
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-12 00:16:32 +0300

    push

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import authRoutes from './routes/authRoutes.js';
 import quizRoutes from './routes/quizRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -12,9 +17,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/quiz', quizRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', userRoutes);
+
+function sendIndex(str) {
+  app.get(str, (_, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+}
+
+sendIndex('/');
+sendIndex('/quiz');
+sendIndex('/admin');
+sendIndex('/login');
 
 export default app;
