abde87b215dd106451dff8efd30c250a5757be1b
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-11 20:50:49 +0300

    push

--- server.js
+++ server.js
@@ -1,49 +1,18 @@
 import app from './app.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import {createAdmin} from './controllers/authController.js';
+import { createAdmin } from './controllers/authController.js';
+
 dotenv.config();
 
-class App {
-  constructor({MONGO_URI, PORT}) {
-    this.PORT = PORT;
-    this.MONGO_URI = MONGO_URI;
-    this.app = app;
+let connected = false;
 
-    this.connect();
+export default async function handler(req, res) {
+  if (!connected) {
+    await mongoose.connect(process.env.MONGO_URI);
+    await createAdmin();
+    connected = true;
   }
 
-  async connect() {
-    try {
-      await mongoose.connect(this.MONGO_URI);
-      await this.run();
-      createAdmin();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async run() {
-    return new Promise((resolve, reject) => {
-      try {
-        this.app.listen(this.PORT, () => {
-          const link = `http://localhost:${this.PORT}`
-          resolve(link);
-          console.log(link);
-        });
-      } catch (error) {
-        console.log(error);
-        reject(null)
-      }
-    })
-  };
-
+  app(req, res);
 }
-
-const wholeApp = new App({
-  MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT
-});
-
-export default wholeApp;
-
