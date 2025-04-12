31d996f7450e178efabb8c7ad98f6c8b83978147
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-13 01:29:47 +0300

    push

--- env.js
+++ env.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log(`vars: `, process.env);
+
 export const PORT = process.env.PORT || 3000;
 export const MONGO_URI = process.env.MONGO_URI || '';
 export const JWT_SECRET = process.env.JWT_SECRET || '';
