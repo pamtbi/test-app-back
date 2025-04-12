71337b773f207359777f9ee6aa35dde59710789b
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-13 00:47:30 +0300

    push

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String },
   password: { type: String },
   role: { type: String, default: 'user' },
   telegramId: { type: Number, unique: true },
